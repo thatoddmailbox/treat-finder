91bfbef2ac45f3d516d6ea17566f4f1635f07db0
GROUP PROJECT <lol.this.is.fake@fakeco.fake>
2015-07-30 14:29:11 +0000

    secrets on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,16 @@
 require 'bundler'
 Bundler.require
 
-require_relative "secrets"
+if ENV["RACK_ENV"] == "production"
+  $GOOGLE_API_KEY = ENV["GOOGLE_API_KEY"]
+  
+  $YELP_CONSUMER_KEY = ENV["YELP_CONSUMER_KEY"]
+  $YELP_CONSUMER_SECRET = ENV["YELP_CONSUMER_SECRET"]
+  $YELP_TOKEN = ENV["YELP_TOKEN"]
+  $YELP_TOKEN_SECRET = ENV["YELP_TOKEN_SECRET"]
+else
+  require_relative "secrets"
+end
 
 configure :development do
   set :database, "sqlite3:db/database.db"
